8ac7c6b84fa80cb72577566cf38d913ad9884921
abhishekkoppu <abhishekkoppu1@gmail.com>
2022-09-13 09:58:55 +0530

    Day 4 pt-1

---
+++ Day 4 Assignment/Part 1/src/BankAccount.java
@@ -0,0 +1,79 @@
+public class BankAccount {
+    private static double totalBalance;
+    private int accountId;
+    private String accountHolderName;
+    private double openingBalance;
+    private double currentBalance;
+
+
+    public BankAccount(int accountId, String accountHolderName, double openingBalance) {
+        if (openingBalance >= 0) {
+            this.accountId = accountId;
+            this.accountHolderName = accountHolderName;
+            this.openingBalance = openingBalance;
+            this.currentBalance = openingBalance;
+            totalBalance += this.currentBalance;
+        } else {
+            System.out.println("Opening Balance should be positive. Please try again");
+        }
+    }
+
+    public void depositAmount(double amount) {
+        if (amount >= 0) {
+
+            this.currentBalance += amount;
+            totalBalance += amount;
+            System.out.println("Amount " + amount + " deposited successfully");
+
+            System.out.println("Account Id: " + this.accountId);
+            System.out.println("Account Holder Name: " + this.accountHolderName);
+            getCurrentBalance();
+            displayTotalBalance();
+            System.out.println("-------------------------------");
+            System.out.println();
+        } else {
+
+            System.out.println("Cannot deposit negative amount");
+            System.out.println("----------------------------------");
+            System.out.println();
+        }
+    }
+
+
+    public void withdrawAmount(double amount) {
+        if (this.currentBalance > amount) {
+
+            this.currentBalance -= amount;
+            totalBalance -= amount;
+            System.out.println("Amount " + amount + " Withdrawn successfully");
+
+            System.out.println("Account Id: " + this.accountId);
+            System.out.println("Account Holder Name: " + this.accountHolderName);
+            getCurrentBalance();
+            displayTotalBalance();
+            System.out.println("-------------------------------");
+            System.out.println();
+
+        } else {
+            getCurrentBalance();
+            System.out.println("Your current balance is less than withdraw amount. Try again");
+            System.out.println("----------------------------------");
+            System.out.println();
+        }
+    }
+
+    public void displayAccountDetails() {
+
+        System.out.println("Your Opening balance: " + this.openingBalance);
+
+
+    }
+
+    public void getCurrentBalance() {
+        System.out.println("Current balance in your account is: " + this.currentBalance);
+    }
+
+    public static void displayTotalBalance() {
+        System.out.println("Total balance in the bank is :" + totalBalance);
+    }
+}
--- Day 4 Assignment/Part 1/src/BankAccountApp.java
+++ Day 4 Assignment/Part 1/src/BankAccountApp.java
@@ -1,9 +1,10 @@
 public class BankAccountApp {
     public static void main(String[] args) {
-    BankAccount bankAccount=new BankAccount("A",500);
-    BankAccount bankAccount1 = new BankAccount("B",1000);
-    BankAccount bankAccount2=new BankAccount("C",0);
+    BankAccount bankAccount=new BankAccount(1,"A",500);
+    BankAccount bankAccount1 = new BankAccount(2,"B",1000);
+    BankAccount bankAccount2=new BankAccount(3,"C",0);
 
+    BankAccount.displayTotalBalance();
     bankAccount.withdrawAmount(100);
     bankAccount1.depositAmount(100);
     bankAccount2.withdrawAmount(100);
