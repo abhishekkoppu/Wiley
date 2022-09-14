7daa7c7c2b9a930dd86d0a910d00a92bd84d7ab1
abhishekkoppu <abhishekkoppu1@gmail.com>
2022-09-14 21:45:50 +0530

    Day 4 pt-2

--- Day 5 Assignment/Part-1/src/Employee.java
+++ Day 5 Assignment/Part-1/src/Employee.java
@@ -53,22 +53,6 @@ public class Employee {
                 +"Employee Department: "+ this.empDept+"\n"+"Employee Gross Salary: "+this.empGrossSalary+"\n";
     }
 
-    public static void main(String[] args) {
-        Employee[] employees=new Employee[5];
-        employees[0]=new Employee(1,"A","Production",1000);
-        employees[1]=new Employee(2,"B","Security",500);
-        employees[2]=new Employee(3,"C","HR",2000);
-        employees[3]=new Employee(4,"D","Production",200);
-        employees[4]=new Employee(5,"E","HR",10000);
-
-        sortEmployees(employees);
-
-        searchEmployees(employees,"HR");
-
-
-
-
-    }
 
 
 }
